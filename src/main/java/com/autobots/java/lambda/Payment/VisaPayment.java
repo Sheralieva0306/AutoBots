71fe714eead23d9d0b48c0c559d296b11a8b6530
Jainagul <sheralieva.0306@gmail.com>
2025-07-09 13:45:23 +0600

    Visa

---
+++ src/main/java/com/autobots/java/lambda/Payment/VisaPayment.java
@@ -0,0 +1,9 @@
+package com.autobots.java.lambda.Payment;
+
+public class VisaPayment {
+
+    public void payByVisaCard(double amount){
+
+        System.out.println("Visa payment by visa card");
+    }
+}
